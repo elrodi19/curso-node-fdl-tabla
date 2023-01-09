097cd4f2f2bbbc7b3e4e8e3cc8f3e48711c69d0c
Fadel <elrodielrodi@gmail.com>
2023-01-09 11:01:10 -0800

    Primer commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('yargs').argv;
+
+// imprimir la tabla del 5.
+
+console.clear();
+
+
+console.log( argv )
+
+
+
+
+
+
+
+//const base = 4;
+
+
+// crearArchivo( base )
+// .then( nombreArchivo => console.log( nombreArchivo, 'creada' ) )
+// .catch( err => console.log( err ) );
